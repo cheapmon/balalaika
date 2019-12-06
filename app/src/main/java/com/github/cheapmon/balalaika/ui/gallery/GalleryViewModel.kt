0306b072bda50d955ccdd02361db2651b9824990
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2019-12-06 18:17:04 +0100

    Application skeleton

---
+++ app/src/main/java/com/github/cheapmon/balalaika/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.github.cheapmon.balalaika.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
