7049ea96eb551e04015789dd373711848e6d5afe
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2019-12-13 12:36:17 +0100

    Switch to Gradle Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,99 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("androidx.navigation.safeargs")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion = "29.0.2"
+
+    defaultConfig {
+        applicationId = "com.github.cheapmon.balalaika"
+        minSdkVersion(15)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(
+            group = "org.jetbrains.kotlin",
+            name = "kotlin-stdlib-jdk7",
+            version = "${rootProject.extra["kotlinVersion"]}"
+    )
+    implementation(
+            group = "androidx.core",
+            name = "core-ktx",
+            version = "1.1.0"
+    )
+    implementation(
+            group = "androidx.legacy",
+            name = "legacy-support-v4",
+            version = "1.0.0"
+    )
+    implementation(
+            group = "com.google.android.material",
+            name = "material",
+            version = "1.0.0"
+    )
+    implementation(
+            group = "androidx.constraintlayout",
+            name = "constraintlayout",
+            version = "1.1.3"
+    )
+    implementation(
+            group = "androidx.navigation",
+            name = "navigation-fragment",
+            version = "2.1.0"
+    )
+    implementation(
+            group = "androidx.navigation",
+            name = "navigation-ui",
+            version = "2.1.0"
+    )
+    implementation(
+            group = "androidx.lifecycle",
+            name = "lifecycle-extensions",
+            version = "2.1.0"
+    )
+    implementation(
+            group = "androidx.navigation",
+            name = "navigation-fragment-ktx",
+            version = "2.1.0"
+    )
+    implementation(
+            group = "androidx.navigation",
+            name = "navigation-ui-ktx",
+            version = "2.1.0"
+    )
+    testImplementation(
+            group = "junit",
+            name = "junit",
+            version = "4.12"
+    )
+    androidTestImplementation(
+            group = "androidx.test.ext",
+            name = "junit",
+            version = "1.1.1"
+    )
+    androidTestImplementation(
+            group = "androidx.test.espresso",
+            name = "espresso-core",
+            version = "3.2.0"
+    )
+}
---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    val kotlinVersion by extra { "1.3.61" }
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath(kotlin("gradle-plugin", kotlinVersion))
+        classpath("com.android.tools.build:gradle:4.0.0-alpha06")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.1.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+apply("tasks.gradle.kts")
