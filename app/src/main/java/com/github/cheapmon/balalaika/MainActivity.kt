ef6ab527c7e4595faae3791100f46e599d7a667d
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2019-12-31 11:27:54 +0100

    display: Remove example fragments

--- app/src/main/java/com/github/cheapmon/balalaika/MainActivity.kt
+++ app/src/main/java/com/github/cheapmon/balalaika/MainActivity.kt
@@ -35,8 +35,7 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
