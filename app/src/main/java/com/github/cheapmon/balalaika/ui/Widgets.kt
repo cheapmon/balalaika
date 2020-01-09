665b709ab9485ff773bf7b4eb4881d66836cc89c
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2020-01-09 12:27:17 +0100

    display: Accept multiple properties per widget

--- app/src/main/java/com/github/cheapmon/balalaika/ui/Widgets.kt
+++ app/src/main/java/com/github/cheapmon/balalaika/ui/Widgets.kt
@@ -2,11 +2,13 @@ package com.github.cheapmon.balalaika.ui
 
 import android.content.Intent
 import android.net.Uri
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.content.ContextCompat.startActivity
 import com.github.cheapmon.balalaika.R
 import com.github.cheapmon.balalaika.ui.home.PropertyLine
@@ -36,53 +38,85 @@ abstract class Widget {
 
 object LexemeWidget : Widget() {
     override fun create(scope: CoroutineScope, group: ViewGroup, line: PropertyLine): View {
-        val view = super.inflate(group, R.layout.lexeme_widget_title)
-        view.findViewById<TextView>(R.id.title).text = line.fullForm.fullForm
-        if (line.fullForm.fullForm != line.property.value) {
-            view.findViewById<TextView>(R.id.lexeme).text = line.property.value
-        } else {
-            view.findViewById<TextView>(R.id.lexeme).text = ""
+        val container = LinearLayoutCompat(group.context).apply {
+            orientation = LinearLayoutCompat.VERTICAL
+            layoutParams = LinearLayoutCompat.LayoutParams(
+                    LinearLayoutCompat.LayoutParams.MATCH_PARENT,
+                    LinearLayoutCompat.LayoutParams.MATCH_PARENT
+            )
         }
-        return view
+        line.properties.forEach { property ->
+            val view = super.inflate(group, R.layout.lexeme_widget_title)
+            view.findViewById<TextView>(R.id.title).text = line.fullForm.fullForm
+            if (line.fullForm.fullForm != property.value) {
+                view.findViewById<TextView>(R.id.lexeme).text = property.value
+            } else {
+                view.findViewById<TextView>(R.id.lexeme).text = ""
+            }
+            container.addView(view)
+        }
+        return container
     }
 }
 
 object PlainWidget : Widget() {
     override fun create(scope: CoroutineScope, group: ViewGroup, line: PropertyLine): View {
-        return super.inflate(group, R.layout.lexeme_widget_plain).apply {
-            this.findViewById<TextView>(R.id.value).text = line.property.value
+        val container = LinearLayoutCompat(group.context).apply {
+            orientation = LinearLayoutCompat.VERTICAL
+            layoutParams = LinearLayoutCompat.LayoutParams(
+                    LinearLayoutCompat.LayoutParams.MATCH_PARENT,
+                    LinearLayoutCompat.LayoutParams.MATCH_PARENT
+            )
         }
+        line.properties.forEach { property ->
+            val view = super.inflate(group, R.layout.lexeme_widget_plain).apply {
+                this.findViewById<TextView>(R.id.value).text = property.value
+            }
+            container.addView(view)
+        }
+        return container
     }
 }
 
 object KeyValueWidget : Widget() {
     override fun create(scope: CoroutineScope, group: ViewGroup, line: PropertyLine): View {
-        val view = super.inflate(group, R.layout.lexeme_widget_key_value)
-        view.findViewById<TextView>(R.id.name).text = line.category
-        view.findViewById<TextView>(R.id.value).text = line.property.value
-        return view
+        val widgetView = super.inflate(group, R.layout.lexeme_widget_key_value)
+        widgetView.findViewById<TextView>(R.id.name).text = line.category
+        val container = widgetView.findViewById<LinearLayoutCompat>(R.id.container)
+        line.properties.forEach { property ->
+            container.addView(TextView(container.context).apply {
+                text = property.value
+                gravity = Gravity.END
+            })
+        }
+        return widgetView
     }
 }
 
 object TextUrlWidget : Widget() {
     override fun create(scope: CoroutineScope, group: ViewGroup, line: PropertyLine): View {
-        val view = super.inflate(group, R.layout.lexeme_widget_text_url)
-        view.findViewById<TextView>(R.id.category).text = line.category
-        val value = line.property.value?.split(Regex(";;;"))?.first()
-        val link = line.property.value?.split(Regex(";;;"))?.getOrNull(1)
-        if (link != null) {
-            view.findViewById<TextView>(R.id.value_with_link).text = value
-            view.findViewById<ImageButton>(R.id.link_btn).setOnClickListener {
-                startActivity(view.context, Intent(Intent.ACTION_VIEW, Uri.parse(link)), null)
+        val widgetView = super.inflate(group, R.layout.lexeme_widget_text_url)
+        widgetView.findViewById<TextView>(R.id.category).text = line.category
+        val container = widgetView.findViewById<LinearLayoutCompat>(R.id.container)
+        line.properties.forEach { property ->
+            val value = property.value?.split(Regex(";;;"))?.first()
+            val link = property.value?.split(Regex(";;;"))?.getOrNull(1)
+            val view = super.inflate(container, R.layout.lexeme_widget_text_url_value)
+            if (link != null) {
+                view.findViewById<TextView>(R.id.value_with_link).text = value
+                view.findViewById<ImageButton>(R.id.link_btn).setOnClickListener {
+                    startActivity(view.context, Intent(Intent.ACTION_VIEW, Uri.parse(link)), null)
+                }
+            } else {
+                view.findViewById<TextView>(R.id.value).apply {
+                    text = value
+                    visibility = View.VISIBLE
+                }
+                view.findViewById<TextView>(R.id.value_with_link).visibility = View.GONE
+                view.findViewById<ImageButton>(R.id.link_btn).visibility = View.GONE
             }
-        } else {
-            view.findViewById<TextView>(R.id.value).apply {
-                text = value
-                visibility = View.VISIBLE
-            }
-            view.findViewById<TextView>(R.id.value_with_link).visibility = View.GONE
-            view.findViewById<ImageButton>(R.id.link_btn).visibility = View.GONE
+            container.addView(view)
         }
-        return view
+        return widgetView
     }
 }
--- app/src/main/java/com/github/cheapmon/balalaika/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/github/cheapmon/balalaika/ui/home/HomeViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.github.cheapmon.balalaika.db.BalalaikaDatabase
-import com.github.cheapmon.balalaika.db.LexemeProperty
 import com.github.cheapmon.balalaika.db.FullForm
+import com.github.cheapmon.balalaika.db.LexemeProperty
 
-data class PropertyLine(val widget: String, val fullForm: FullForm, val category: String, val property: LexemeProperty)
+data class PropertyLine(val widget: String, val fullForm: FullForm, val category: String, val properties: List<LexemeProperty>)
 data class DictionaryEntry(val fullForm: FullForm, val lines: List<PropertyLine>)
 
 class HomeViewModel() : ViewModel() {
@@ -16,13 +16,16 @@ class HomeViewModel() : ViewModel() {
 
     init {
         val factory = BalalaikaDatabase.instance.fullFormDao().getAll().map { lexeme ->
-            val properties = BalalaikaDatabase.instance.lexemePropertyDao().findByFullForm(lexeme.id).map { property ->
-                val category = BalalaikaDatabase.instance.categoryDao().findById(property.categoryId)
-                val widget = category?.widget ?: "plain"
-                val categoryName = category?.name ?: ""
-                PropertyLine(widget, lexeme, categoryName, property)
-            }
-            DictionaryEntry(lexeme, properties)
+            val lines = BalalaikaDatabase.instance.lexemePropertyDao()
+                    .findByFullForm(lexeme.id)
+                    .groupBy { it.categoryId }
+                    .map { (categoryId, properties) ->
+                        val category = BalalaikaDatabase.instance.categoryDao().findById(categoryId)
+                        val widget = category?.widget ?: "plain"
+                        val categoryName = category?.name ?: ""
+                        PropertyLine(widget, lexeme, categoryName, properties)
+                    }
+            DictionaryEntry(lexeme, lines)
         }
         lexemes = LivePagedListBuilder(factory, 10).build()
     }
