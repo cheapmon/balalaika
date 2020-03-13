596051ec7f0ab4bc51475300311c193584ea2964
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2020-03-13 15:20:54 +0100

    restart: Create application skeleton

---
+++ app/src/main/java/com/github/cheapmon/balalaika/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.github.cheapmon.balalaika
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.github.cheapmon.balalaika.databinding.ActivityMainBinding
+import com.github.cheapmon.balalaika.databinding.AppBarMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val activityBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val barBinding: AppBarMainBinding = activityBinding.appBarMain
+
+        setSupportActionBar(barBinding.toolbar)
+        val drawerLayout: DrawerLayout = activityBinding.drawerLayout
+        val navView: NavigationView = activityBinding.navView
+
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
