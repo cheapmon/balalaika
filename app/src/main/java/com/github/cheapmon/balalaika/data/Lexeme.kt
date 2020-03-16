74858e6ed3ecadd5713153b2271a14e620999df6
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2020-03-16 15:36:37 +0100

    data: Add Lexeme -> Lexeme relation

--- app/src/main/java/com/github/cheapmon/balalaika/data/Lexeme.kt
+++ app/src/main/java/com/github/cheapmon/balalaika/data/Lexeme.kt
@@ -3,16 +3,26 @@ package com.github.cheapmon.balalaika.data
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(entity = Lexeme::class, parentColumns = ["id"], childColumns = ["base_id"])
+    ],
+    indices = [
+        Index(value = ["external_id"], unique = true),
+        Index(value = ["base_id"])
+    ]
+)
 data class Lexeme(
-        @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val lexemeId: Long = 0,
-        @ColumnInfo(name = "form") val form: String,
-        @ColumnInfo(name = "base_id") val baseId: Long?
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val lexemeId: Long = 0,
+    @ColumnInfo(name = "external_id") val externalId: String,
+    @ColumnInfo(name = "form") val form: String,
+    @ColumnInfo(name = "base_id") val baseId: Long?
 )
 
-data class LexemeWithProperties(
-        @Embedded val lexeme: Lexeme,
-        @Relation(parentColumn = "id", entityColumn = "lexeme_id") val properties: List<Property>
+data class LexemeWithRelations(
+    @Embedded val lexeme: Lexeme,
+    @Relation(parentColumn = "base_id", entityColumn = "id") val base: Lexeme?,
+    @Relation(parentColumn = "id", entityColumn = "lexeme_id") val properties: List<Property>
 )
 
 @Dao
@@ -22,7 +32,7 @@ interface LexemeDao {
 
     @Transaction
     @Query("SELECT * FROM lexeme")
-    fun getAllWithProperties(): Flow<List<LexemeWithProperties>>
+    fun getAllWithRelations(): Flow<List<LexemeWithRelations>>
 
     @Query("SELECT COUNT(*) FROM lexeme")
     fun count(): Flow<Int>
