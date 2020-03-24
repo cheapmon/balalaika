95b11eebcf99225dd93fee2026c2031a42f17b5e
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2020-03-24 13:38:43 +0100

    search: Navigate from Dictionary

--- app/src/main/java/com/github/cheapmon/balalaika/ui/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/github/cheapmon/balalaika/ui/dictionary/DictionaryFragment.kt
@@ -1,11 +1,10 @@
 package com.github.cheapmon.balalaika.ui.dictionary
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.github.cheapmon.balalaika.R
 import com.github.cheapmon.balalaika.util.InjectorUtil
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -23,6 +22,23 @@ class DictionaryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_dictionary, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_dictionary, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.go_to_search -> {
+                val directions = DictionaryFragmentDirections.actionNavHomeToNavSearch()
+                findNavController().navigate(directions)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
